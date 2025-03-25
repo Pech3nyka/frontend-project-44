63f97ed854149494c414d76b9eb7860fc11271d6
Pech3nyka <pilaeff2017@yandex.ru>
2025-03-25 12:02:00 +0300

    brain games

--- src/index.js
+++ src/index.js
@@ -4,25 +4,21 @@ import greetUser from './cli.js';
 const runGame = (gameDescription, generateQuestionAndAnswer) => {
   greetUser();
   
-  const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}!`);
-  
   console.log(gameDescription);
 
   for (let correctAnswers = 0; correctAnswers < 3; correctAnswers += 1) {
     const [question, correctAnswer] = generateQuestionAndAnswer();
-    console.log(`Question: ${question}`);
-    const userAnswer = readlineSync.question('Your answer: ');
+    const userAnswer = readlineSync.question(`Question: ${question} `);
 
     if (userAnswer !== correctAnswer) {
       console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-      console.log(`Let's try again, ${name}!`);
+      console.log('Let\'s try again!');
       return;
     }
     console.log('Correct!');
   }
 
-  console.log(`Congratulations, ${name}!`);
+  console.log('Congratulations!');
 };
 
 export default runGame;
