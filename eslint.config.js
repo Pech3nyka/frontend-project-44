7f87ce2081e2871c14a65fb943fff7750787fb5f
Pech3nyka <pilaeff2017@yandex.ru>
2025-03-25 10:43:59 +0300

    brain games

--- eslint.config.js
+++ eslint.config.js
@@ -6,6 +6,10 @@ export default [
     languageOptions: {
       globals: globals.node,
     },
+    rules: {
+      'indent': ['error', 2],
+      'arrow-body-style': ['error', 'as-needed'],
+    },
   },
   pluginJs.configs.recommended,
 ];
